be7222780dd482f182906f463145a0f4c26e820e
Pablo Martin Morenos <pablomartinmoreno@protonmail.com>
2023-07-15 19:30:31 -0300

    Se ejecuta infinitamente cada 30 minutos

--- final_bonos_and_cer.py
+++ final_bonos_and_cer.py
@@ -4,6 +4,9 @@ import time
 # La lista de tus scripts
 scripts = ["main.py", "mainbonos.py", "tickers_and_cer.py"]
 
-for script in scripts:
-    # subprocess.call ejecuta el script y espera a que termine
-    subprocess.call(["python", script])
+while True:  # Bucle infinito
+    for script in scripts:
+        # subprocess.call ejecuta el script y espera a que termine
+        subprocess.call(["python", script])
+    
+    time.sleep(1800)  # Espera 30 minutos (1800 segundos)
